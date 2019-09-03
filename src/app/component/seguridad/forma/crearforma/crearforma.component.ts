db10800b8ce6783773f3acd4d5faccb01c492958
Kevin De Alba P <kdealba1@cuc.edu.co>
2019-09-03 14:56:29 -0500

    Launching

--- src/app/component/seguridad/forma/crearforma/crearforma.component.ts
+++ src/app/component/seguridad/forma/crearforma/crearforma.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-crearforma',
@@ -6,12 +9,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crearforma.component.css']
 })
 export class CrearformaComponent implements OnInit {
+  
+  nombre : string;
+  icono : string;
+  firstFormGroup    : FormGroup;
 
-  constructor() { }
+  constructor(private _formBuilder: FormBuilder, public dialog: MatDialog, private router : Router,  private _location: Location) { }
 
   ngOnInit() {
-  }
+
+    this.firstFormGroup = this._formBuilder.group({
+
+nombre :['',Validators.required,Validators.minLength(11)],
+icono  :['',Validators.required,Validators.minLength(12)],
+
+                                               
+
+
+
+
+
+
+
+
+  });
 
 }
 
 
+guardarMenu(){
+  
+}
+
+}
